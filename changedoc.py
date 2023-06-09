629e3477c5f63dad566a30cff51ac53b29fc022a
shac92 <107327791+shac92@users.noreply.github.com>
2023-06-09 18:34:22 -0400

    Update changedoc.py

--- changedoc.py
+++ changedoc.py
@@ -7,9 +7,10 @@ def rename_files(folder_path):
     for filename in files:
         # Check if the file has the .jpg_ extension
         if filename.endswith('.jpg_'):
+                                #^This will be what the script looks for change it to what you need it to look for.
             # Generate the new filename by replacing .jpg with .jpeg
             new_filename = filename.replace('.jpg_', '.jpeg')
-
+                                            #^change these^ to the old extention type and the new extention type.
             # Rename the file
             old_path = os.path.join(folder_path, filename)
             new_path = os.path.join(folder_path, new_filename)
@@ -17,7 +18,7 @@ def rename_files(folder_path):
             print(f"Renamed: {filename} -> {new_filename}")
 
 # Specify the folder path where the files are located
-folder_path = 'C:\\Users\shafe\Desktop\\New folder (2)\\Download'
+folder_path = '*insert new folder pather here'
 
 # Call the function to rename the files
 rename_files(folder_path)
